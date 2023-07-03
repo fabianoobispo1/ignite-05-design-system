c40e21f9ec83524a039749ccca6eac164f98b261
fabianoobispo1 <fabianoobispo@icloud.com>
2023-07-02 21:35:08 -0300

    add

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,19 @@
+import { UserFocus } from 'phosphor-react'
+import { ComponentProps } from 'react'
+import { AvatarContainer, AvatarImage, AvatarFallback } from './styles'
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+
+      <AvatarFallback delayMs={600}>
+        <UserFocus />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
+
+Avatar.displayName = 'Avatar'
