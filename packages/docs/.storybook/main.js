cb2559c81dedd2e126820c3ee344cdf6b8d2d82c
fabianoobispo1 <fabianoobispo@icloud.com>
2023-07-03 19:08:18 -0300

    add

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -2,12 +2,15 @@
 const config = {
   stories: [ 
     "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.tsx"
+    "../src/stories/**/*.stories.tsx",
+    '../src/**/*.mdx',
   ],
    addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y"
   ],
   framework: {
     name: "@storybook/react-vite",
